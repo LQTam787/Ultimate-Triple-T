9723bc6e4935dd742d842c9bfa12012671dffd70
Quang Tam Lai <lqtam3004@gmail.com>
2023-11-05 08:57:36 +0700

    Đã xong GUI cho UTTT
    UTTT đã có GUI rồi, cậu chạy thử xem

---
+++ Main.java
@@ -0,0 +1,40 @@
+import java.awt.EventQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.UIManager.LookAndFeelInfo;
+
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        JOptionPane.showMessageDialog(null, "Welcome to Ultimate Tic-Tac-Toe! LET'S GET IT ON!!!");
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(HUD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        
+        //</editor-fold>
+
+        /* Create and display the form */
+        EventQueue.invokeLater(() -> {
+            new HUD().setVisible(true);
+        });
+    }
+}
